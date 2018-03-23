3de83702439539da99d1a3572803689d6fe46d80
Kristof Niederholtmeyer <kristof.niederholtmeyer@rwth-aachen.de>
2018-03-23 19:10:12 +0100

    Add evil package but do not enable it by default

--- init.el
+++ init.el
@@ -11,7 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit p4 gruvbox-theme))))
+ '(package-selected-packages (quote (evil magit p4 gruvbox-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
