f02cb52d5c3f6e6bce3de6eea45ca0a5efe7ef86
Kristof Niederholtmeyer <kristof@Kristofs-MacBook-Air.fritz.box>
2021-04-03 11:46:47 +0200

    Align code indentation

--- init.el
+++ init.el
@@ -11,8 +11,26 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(magit-log-section-arguments (quote ("--graph" "--color" "--decorate" "-n256")))
- '(package-selected-packages (quote (evil magit p4 gruvbox-theme))))
+ '(magit-log-section-arguments '("--graph" "--color" "--decorate" "-n256"))
+ '(package-selected-packages '(verilog-mode evil magit p4 gruvbox-theme))
+ '(verilog-align-ifelse t)
+ '(verilog-auto-delete-trailing-whitespace t)
+ '(verilog-auto-inst-param-value t)
+ '(verilog-auto-inst-vector nil)
+ '(verilog-auto-lineup 'all)
+ '(verilog-auto-newline nil)
+ '(verilog-auto-save-policy nil)
+ '(verilog-auto-template-warn-unused t)
+ '(verilog-case-indent 2)
+ '(verilog-cexp-indent 2)
+ '(verilog-highlight-grouping-keywords t)
+ '(verilog-highlight-modules t)
+ '(verilog-indent-level 2)
+ '(verilog-indent-level-behavioral 2)
+ '(verilog-indent-level-declaration 2)
+ '(verilog-indent-level-module 2)
+ '(verilog-indent-lists nil)
+ '(verilog-tab-to-comment nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
